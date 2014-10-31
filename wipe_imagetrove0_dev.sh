1e333dbb0e0e5757931130fbd8f50735d3b75738
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2014-10-31 04:20:38 +0000

    Update wipe script for dev environment.

--- wipe_imagetrove0_dev.sh
+++ wipe_imagetrove0_dev.sh
@@ -1,4 +1,5 @@
 sudo chown -R carlo.carlo /data/Q0107/imagetrove0_dev_volumes/*
+find /data/Q0107/imagetrove0_dev_volumes/var_log/ -type f -exec rm {} \;
 rm -fr /data/Q0107/imagetrove0_dev_volumes/data/*
 rm -fr /data/Q0107/imagetrove0_dev_volumes/mytardis_staging/*
 rm -fr /data/Q0107/imagetrove0_dev_volumes/mytardis_store/*
