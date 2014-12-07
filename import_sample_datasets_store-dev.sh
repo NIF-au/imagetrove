8e26e554eaf7f32ba1b0b954609b1ed0b9055113
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2014-12-08 09:39:47 +1000

    Add import script for sample data.

---
+++ import_sample_datasets_store-dev.sh
@@ -0,0 +1,7 @@
+./import_into_orthanc.py localhost 8043 /export/nif02/imagetrove/data_Q0107_imagetrove_datasets/carlo_brain/
+./import_into_orthanc.py localhost 8043 /export/nif02/imagetrove/data_Q0107_imagetrove_datasets/nif_samples/cmca/CMCA_WA__BioSpec_heartslice/
+./import_into_orthanc.py localhost 8043 /export/nif02/imagetrove/data_Q0107_imagetrove_datasets/nif_samples_modified/cmca/CMCA_WA__Skyscan1176_MicroCT__Femur_DicomFormat
+./import_into_orthanc.py localhost 8043 /export/nif02/imagetrove/data_Q0107_imagetrove_datasets/nif_samples/cmca/CMCA_WA__Versa520_MicroCT__Rock_Dicom_Format
+./import_into_orthanc.py localhost 8043 /export/nif02/imagetrove/data_Q0107_imagetrove_datasets/nif_samples/Dicom_SAHMRI/
+./import_into_orthanc.py localhost 8043 /export/nif02/imagetrove/data_Q0107_imagetrove_datasets/nif_samples/DICOM-UNSWNeuRA/
+./import_into_orthanc.py localhost 8043 /export/nif02/imagetrove/data_Q0107_imagetrove_datasets/nif_samples/MBI_dicom_samples/
